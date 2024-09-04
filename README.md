9eb80eb608ebcde0dad2ca2c1a938171a27eb6c0
Gilbert Haro <gilbertharocode@gmail.com>
2024-09-03 19:58:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Worldwise
+ Keep track of your adventures - React Router
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/HomePage';
+import AppLayout from './pages/AppLayout';
+import './App.css';
+import PageNotFound from './pages/Pagenotfound';
+
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path='product' element={<Product />} />
+          <Route path='pricing' element={<Pricing />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='app' element={<AppLayout />}>
+            <Route index element={<p> List of cities</p>} />
+            <Route path='cities' element={<p>List of Cities</p>} />
+            <Route path='countries' element={<p>List of countries</p>} />
+            <Route path='form' element={<p>Form</p>} />
+          </Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
