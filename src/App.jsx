8ccb88b6dbfcf87046dd2e2cbcd95416a67c5422
Gilbert Haro <gilbertharocode@gmail.com>
2024-09-03 23:34:19 -0700

    city list and item component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,36 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/HomePage';
+import CityList from './components/CityList';
 import AppLayout from './pages/AppLayout';
 import './App.css';
 import PageNotFound from './pages/Pagenotfound';
+import { useEffect, useState } from 'react';
 
 import Login from './pages/Login';
 
+const BASE_URL = 'http://localhost:9000';
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('There was an error loading data');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -18,8 +41,14 @@ function App() {
           <Route path='pricing' element={<Pricing />} />
           <Route path='/login' element={<Login />} />
           <Route path='app' element={<AppLayout />}>
-            <Route index element={<p> List of cities</p>} />
-            <Route path='cities' element={<p>List of Cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path='cities'
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path='countries' element={<p>List of countries</p>} />
             <Route path='form' element={<p>Form</p>} />
           </Route>
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,33 @@
+import styles from './CityItem.module.css';
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date));
+
+// function City() {
+//   // TEMP DATA
+//   const currentCity = {
+//     cityName: 'Lisbon',
+//     emoji: '🇵🇹',
+//     date: '2027-10-31T15:59:59.138Z',
+//     notes: 'My favorite city so far!',
+//   };
+// }
+
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+    </li>
+  );
+}
+
+export default CityItem;
