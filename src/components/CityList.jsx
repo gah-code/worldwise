64cbe9d3542b211e7af94ab848df13b3f0087b74
Gilbert Haro <gilbertharocode@gmail.com>
2024-09-13 16:42:24 -0700

    programmatic navigation with useNavigation, Dynamic Routes, and useContext code refactor

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,9 +2,12 @@ import styles from './CityList.module.css';
 import Spinner from './Spinner';
 import CityItem from './CityItem';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 // import PropTypes from 'prop-types';
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,20 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}>app</div>;
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
