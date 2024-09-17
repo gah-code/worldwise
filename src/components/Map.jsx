dd2449aa2911f8b403391a772b64855938542cca
Gilbert Haro <gilbertharocode@gmail.com>
2024-09-17 16:55:40 -0700

    useContext hook and leaflet api npm

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,34 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
 import styles from './Map.module.css';
 
 function Map() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   // eslint-disable-next-line no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  // const lat = searchParams.get('lat');
+  // const lng = searchParams.get('lng');
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
